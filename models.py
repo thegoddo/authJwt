5bf8b33bb1a3c8f4d8e4d0f92dfb2a7cbe6abe55
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-19 12:43:39 +0530

    Task model

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Integer
+from sqlalchemy import ForeignKey, String, Integer, Text
 from sqlalchemy.orm import mapped_column, Mapped
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -18,3 +18,8 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+class Task(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
