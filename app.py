a310d6681b46be30d8a83f12d3479f093adfbda9
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-19 13:13:01 +0530

    refactor app.py to improve structure and ensure database creation within app context

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+from app import create_app, db
 
-from flask import Flask, request, jsonify
-from flask_bcrypt import Bcrypt
-from bson import ObjectId
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+app = create_app()
 
+with app.app_context():
+    db.create_all()
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
