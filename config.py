cf93b0f4e82cb757e33e585388b4362635a67dcb
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-19 14:53:57 +0530

    add: set JWT_VERIFY_SUB to bypass subject string requirement

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY=os.environ.get("SECRET_KEY")
     JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY")
     basedir=os.path.abspath(os.path.dirname(__file__))
+    JWT_VERIFY_SUB=False # Bypasses subject must be string
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
             'sqlite:///'+os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
