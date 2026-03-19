49c010a06cf3bed7182c49406a809ae79ddfeffc
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-03-19 14:54:04 +0530

    add: improve logging setup in application initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
@@ -7,18 +8,22 @@ from config import Config
 from app.models import db
 jwt = JWTManager()
 
+
 def create_app(config_class=Config):
-    app  = Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object(config_class)
-    
+    logging.basicConfig(filename='app.log', level=logging.INFO,
+                        format='%(asctime)s %(levelname)s: %(message)s')
+    app.logger.info("Primetrade API starting up...")
+
     db.init_app(app)
     jwt.init_app(app)
     CORS(app)
-    
+
     from app.api.v1_auth import auth_bp
     from app.api.v1_tasks import tasks_bp
-    
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(tasks_bp)
-    
+
     return app
